ac07112d82accf54c18e5e0c5e828532c368ba22
Prabhat kumar <114340301+freaknu@users.noreply.github.com>
2024-03-27 00:56:14 +0530

    Update LinkedList_1.cpp

--- LinkedList_1.cpp
+++ LinkedList_1.cpp
@@ -1,109 +1,95 @@
-class Node
-{
+class Node {
 public:
     int data;
     Node *next;
-    Node(int data)
-    {
+    Node(int data) {
         this->data = data;
         this->next = nullptr;
     }
-   ~Node()
-    {
-        int value = this->data;
-        if (this->next != NULL)
-        {
-           delete next;
-           next = NULL;
-        }
-        cout << "Memory Freed for: " << value << endl;
+    ~Node() {
+        cout << "Memory Freed for: " << data << endl;
+        delete next;
     }
 };
 
 class LinkedList {
 public:
-    void InsertNodeAtHead(Node *&head, int data)
-    {
+    void InsertNodeAtHead(Node *&head, int data) {
         Node *temp = new Node(data);
         temp->next = head;
         head = temp;
     }
 
-    void InsertNodeAtTail(Node *&tail, int data)
-    {
+    void InsertNodeAtTail(Node *&tail, int data) {
         Node *temp = new Node(data);
-        tail->next = temp;
-        tail = tail->next;
+        if (tail == nullptr) {
+            tail = temp;
+        } else {
+            tail->next = temp;
+            tail = temp;
+        }
     }
 
-    void PrintNode(Node *&head)
-    {
+    void PrintNode(Node *head) {
         Node *temp = head;
-        while (temp != nullptr)
-        {
+        while (temp != nullptr) {
             cout << temp->data << " ";
             temp = temp->next;
         }
         cout << endl;
     }
 
-    void InsertAtPosition(Node *&head, int Position, int data)
-    {
+    void InsertAtPosition(Node *&head, int Position, int data) {
+        if (Position <= 0) {
+            cout << "Invalid position." << endl;
+            return;
+        }
         Node *node = new Node(data);
-        if (Position == 1)
-        {
+        if (Position == 1) {
             InsertNodeAtHead(head, data);
             return;
         }
         Node *temp = head;
         int cnt = 1;
-        while (cnt < Position - 1 && temp != nullptr)
-        {
+        while (cnt < Position - 1 && temp != nullptr) {
             temp = temp->next;
             cnt++;
         }
-        if (temp == nullptr)
-        {
+        if (temp == nullptr) {
             cout << "Position out of range." << endl;
             return;
         }
         node->next = temp->next;
         temp->next = node;
     }
-void DeleteNode(Node *&head, int Pos)
-{
-    if (head == NULL)
-    {
-        cout << "List is empty." << endl;
-        return;
-    }
 
-    if (Pos == 1)
-    {
-        Node *temp = head;
-        head = head->next;
-        temp->next = NULL;
-        delete temp;
-    }
-    else
-    {
-        Node *currentNode = head;
-        Node *PreviousNode = NULL;
-        int Count = 1;
-        while (currentNode != NULL && Count < Pos)
-        {
-            PreviousNode = currentNode;
-            currentNode = currentNode->next;
-            Count++;
-        }
-        if (currentNode == NULL)
-        {
-            cout << "Position out of range." << endl;
+    void DeleteNode(Node *&head, int Pos) {
+        if (head == nullptr) {
+            cout << "List is empty." << endl;
             return;
         }
-        PreviousNode->next = currentNode->next;
-        currentNode->next = NULL;
-        delete currentNode;
+
+        if (Pos == 1) {
+            Node *temp = head;
+            head = head->next;
+            temp->next = nullptr;
+            delete temp;
+        } else {
+            Node *currentNode = head;
+            Node *PreviousNode = nullptr;
+            int Count = 1;
+            while (currentNode != nullptr && Count < Pos) {
+                PreviousNode = currentNode;
+                currentNode = currentNode->next;
+                Count++;
+            }
+            if (currentNode == nullptr) {
+                cout << "Position out of range." << endl;
+                return;
+            }
+            PreviousNode->next = currentNode->next;
+            currentNode->next = nullptr;
+            delete currentNode;
+        }
     }
-  }
 };
