e93fcc3a70b47bc887b2e55719f6102a15b9ed1f
Prabhat kumar <114340301+freaknu@users.noreply.github.com>
2024-03-27 00:48:39 +0530

    Add files via upload

---
+++ LinkedList_1.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int data)
+    {
+        this->data = data;
+        this->next = nullptr;
+    }
+};
+
+class LinkedList {
+public:
+    void InsertNodeAtHead(Node *&head, int data)
+    {
+        Node *temp = new Node(data);
+        temp->next = head;
+        head = temp;
+    }
+
+    void InsertNodeAtTail(Node *&tail, int data)
+    {
+        Node *temp = new Node(data);
+        tail->next = temp;
+        tail = tail->next;
+    }
+
+    void PrintNode(Node *&head)
+    {
+        Node *temp = head;
+        while (temp != nullptr)
+        {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+
+    void InsertAtPosition(Node *&head, int Position, int data)
+    {
+        Node *node = new Node(data);
+        if (Position == 1)
+        {
+            InsertNodeAtHead(head, data);
+            return;
+        }
+        Node *temp = head;
+        int cnt = 1;
+        while (cnt < Position - 1 && temp != nullptr)
+        {
+            temp = temp->next;
+            cnt++;
+        }
+        if (temp == nullptr)
+        {
+            cout << "Position out of range." << endl;
+            return;
+        }
+        node->next = temp->next;
+        temp->next = node;
+    }
+};
